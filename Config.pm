6bc03d60c2f1a9937f0c14c57526f016a18d8555
Pami Ketolainen <pami.ketolainen@jolla.com>
2017-10-19 12:44:34 +0300

    Update headers

--- Config.pm
+++ Config.pm
@@ -4,8 +4,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# Copyright (C) 2012 Jolla Ltd.
-# Contact: Pami Ketolainen <pami.ketolainen@jollamobile.com>
+# Copyright (C) 2012-2017 Jolla Ltd.
+# Contact: Pami Ketolainen <pami.ketolainen@jolla.com>
 
 package Bugzilla::Extension::BugViewPlus;
 use strict;
